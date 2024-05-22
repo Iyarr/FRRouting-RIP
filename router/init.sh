4c1aaaa5b0d65b890eaad2b4cbcb7f7ca684f585
Iyarr <8.24ridiculous@gmail.com>
2024-05-22 10:26:10 +0900

    コマンドの改良

--- router/init.sh
+++ router/init.sh
@@ -12,18 +12,9 @@ router rip
 version $RIP_VERSION" > /home/commands
 # ホスト内のIFとIPアドレスを取得
 ip address show | grep "scope global" | while read line; do
-  # ブロードキャストアドレスを取得
-  brd_address=$(echo "$line" | grep -oE 'brd\s[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-  # ブロードキャストアドレスが10.0.0.255か10.0.5.255の場合はRIPを有効にしない
-  # 詳しくは rip.md の構成図を参照
-  if [ "$brd_address" != "brd 10.0.0.255" ] && [ "$brd_address" != "brd 10.0.5.255" ]; then
-    # インタフェース名を取得
-    ifname=$(echo "$line" | grep -oE '[A-Za-z0-9]*$')
-    echo "network $ifname" >> /home/commands
-  else 
-    ifname=$(echo "$line" | grep -oE '[A-Za-z0-9]*$')
-    echo "network $ifname" >> /home/commands
-  fi
+  # それぞれのインタフェースでRIPを有効化
+  ifname=$(echo "$line" | grep -oE '[A-Za-z0-9]*$')
+  echo "network $ifname" >> /home/commands
 done
 
 echo "#! /bin/bash 
